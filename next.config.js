0692e8072a17ab073eefe95a08873f45a60c3750
anjavojta1993 <anja.vojta@gmail.com>
2021-05-30 22:06:39 +0200

    Add library to convert svg file

--- next.config.js
+++ next.config.js
@@ -1,2 +1,7 @@
 const withImages = require('next-images');
-module.exports = withImages();
+module.exports = withImages({
+  fileExtensions: ['jpg', 'jpeg', 'png', 'gif', 'svg'],
+  webpack(config, options) {
+    return config;
+  },
+});
