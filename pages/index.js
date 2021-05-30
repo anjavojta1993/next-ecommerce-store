7a22535681b3b0915339f23fb390ba85a194352a
anjavojta1993 <anja.vojta@gmail.com>
2021-05-30 20:38:16 +0200

    Remove unused dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
