9e7834227460f827e085d34270abf0ded02d1f8a
anjavojta1993 <anja.vojta@gmail.com>
2021-05-31 18:00:25 +0200

    Update database

--- util/database.js
+++ util/database.js
@@ -1,5 +1,26 @@
 export const products = [
-  { id: '1', name: 'Basil' },
-  { id: '2', name: 'Oregano' },
-  { id: '3', name: 'Sage' },
+  {
+    id: '1',
+    name: 'Organic Basil',
+    price: '2,99€',
+    image: 'images/basil.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    id: '2',
+    name: 'Organic Rosemary',
+    price: '3,99€',
+    image: 'images/rosemary.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    id: '3',
+    name: 'Organic Sage',
+    price: '3,99€',
+    image: 'images/sage.jpg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
 ];
