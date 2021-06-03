24e74831f1e2932082c1ba0308d972bb68e4b37e
anjavojta1993 <anja.vojta@gmail.com>
2021-06-03 18:56:15 +0200

    Update footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,14 @@
 import { css } from '@emotion/react';
 import Link from 'next/link';
-import { darkGrey } from '../pages/_app';
+import { lightGrey } from '../pages/_app';
 
 const footerStyles = css`
   display: flex;
   align-items: center;
   padding: 10px 15px;
-  background-color: ${darkGrey};
-  position: absolute;
-  bottom: 0;
+  background-color: ${lightGrey};
+  /* position: absolute;
+  bottom: 0; */
   width: 100%;
   height: 50px;
 
@@ -17,7 +17,7 @@ const footerStyles = css`
     text-transform: uppercase;
     letter-spacing: 1px;
     text-decoration: none;
-    color: white;
+    color: black;
     font-weight: bold;
     overflow: hidden;
 
