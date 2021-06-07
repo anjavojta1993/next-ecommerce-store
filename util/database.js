d75b8a2910323c5a5e568f4f24bf6a57e9975121
anjavojta1993 <anja.vojta@gmail.com>
2021-06-07 19:35:44 +0200

    Update database

--- util/database.js
+++ util/database.js
@@ -1,8 +1,54 @@
+import camelcaseKeys from 'camelcase-keys';
+import dotenvSafe from 'dotenv-safe';
+import postgres from 'postgres';
+
+// Read the PostgreSQL secret connection information
+// (host, database, username password) from the .env file
+dotenvSafe.config();
+
+function connectOneTimeToDatabase() {
+  let sql;
+  if (process.env.NODE_ENV === 'production') {
+    // Heroku needs SSL connections but
+    //  has an unauthorized certificate
+    // https://devcenter.heroku.com/changelog-items/852
+    sql = postgres({ ssl: { rejectUnauthorized: false } });
+  } else {
+    if (!globalThis.__postgresSqlClient) {
+      globalThis.__postgresSqlClient = postgres();
+    }
+    sql = globalThis.__postgresSqlClient;
+  }
+  return sql;
+}
+
+// Connect to PostgreSQL
+const sql = connectOneTimeToDatabase();
+
+// Perform a first query
+export async function getProducts() {
+  const products = await sql`SELECT * FROM products`;
+  return products.map((product) => camelcaseKeys(product));
+}
+
+export async function getProductById(id) {
+  const productsList = await sql`
+  SELECT
+  *
+  FROM
+  products
+  WHERE
+  id = ${id}
+  `;
+  return productsList.map((product) => camelcaseKeys(product))[0];
+}
+
 export const products = [
   {
     id: '1',
     name: 'Organic Basil',
-    price: '2,99€',
+    price: '2,99',
+    currency: '€',
     image: '/images/basil.jpg',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -10,7 +56,8 @@ export const products = [
   {
     id: '2',
     name: 'Organic Rosemary',
-    price: '3,99€',
+    price: '3,99',
+    currency: '€',
     image: '/images/rosemary.jpg',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -18,7 +65,8 @@ export const products = [
   {
     id: '3',
     name: 'Organic Sage',
-    price: '3,99€',
+    price: '3,99',
+    currency: '€',
     image: '/images/sage.jpg',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
