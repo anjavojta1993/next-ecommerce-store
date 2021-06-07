b587710341f0fa990c37d3ebc6749fa499304ef4
anjavojta1993 <anja.vojta@gmail.com>
2021-06-07 15:57:39 +0200

    Add database

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ export const products = [
     id: '1',
     name: 'Organic Basil',
     price: '2,99€',
-    image: 'images/basil.jpg',
+    image: '/images/basil.jpg',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
@@ -11,7 +11,7 @@ export const products = [
     id: '2',
     name: 'Organic Rosemary',
     price: '3,99€',
-    image: 'images/rosemary.jpg',
+    image: '/images/rosemary.jpg',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
@@ -19,7 +19,7 @@ export const products = [
     id: '3',
     name: 'Organic Sage',
     price: '3,99€',
-    image: 'images/sage.jpg',
+    image: '/images/sage.jpg',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
