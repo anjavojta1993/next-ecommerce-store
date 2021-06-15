3a6f52357c3e06338ba056ec29d73345f936e981
anjavojta1993 <anja.vojta@gmail.com>
2021-06-15 17:51:30 +0200

    Update cypress quantity

--- cypress/integration/changeQuantity.spec.js
+++ cypress/integration/changeQuantity.spec.js
@@ -8,8 +8,10 @@ describe('update quantity', () => {
     cy.get('[data-cy="single-product-learn-more"]').first().click();
     // check if quantity selection is visible
     cy.get('[data-cy="quantity-input-dropdown"]').should('be.visible');
-    // select quantity of 5
-    cy.get('[data-cy="quantity-input-dropdown"]').type('{downarrow}');
+    // delete quantity
+    cy.get('[data-cy="quantity-input-dropdown"]').clear();
+    // type quantity of 5
+    cy.get('[data-cy="quantity-input-dropdown"]').type('5');
     // check if add to cart button is visible
     cy.get('[data-cy="single-product-add-to-cart-button"]').should(
       'be.visible',
@@ -20,10 +22,10 @@ describe('update quantity', () => {
     cy.get('[data-cy="navigate-to-cart"]').click();
     // check if quantity selection is visible
     cy.get('[data-cy="quantity-input-dropdown-cart"]').should('be.visible');
-    // select quantity of 2
-    cy.get('[data-cy="quantity-input-dropdown-cart"]').trigger('pointerdown', {
-      which: 2,
-    });
+    // delete quantity
+    cy.get('[data-cy="quantity-input-dropdown"]').clear();
+    // type quantity of 2
+    cy.get('[data-cy="quantity-input-dropdown"]').type('2');
     // check if remove button is visible
     cy.get('[data-cy="remove-button-cart"]').should('be.visible');
     // click on remove button
