f1416dc60eba3b385c7ebdb66cf555694c2a237e
anjavojta1993 <anja.vojta@gmail.com>
2021-06-16 17:35:51 +0200

    Add shared styles

---
+++ util/sharedStyles.js
@@ -0,0 +1,9 @@
+// color palette of application
+
+export const darkBrown = '#6A300C';
+export const rose = '#DF846E';
+export const lightRose = '#FBF1EE';
+export const darkGreen = '#293A11';
+export const lightGreen = '#858D3B';
+export const darkGrey = '#68666B';
+export const lightGrey = '#D6D5D7';
